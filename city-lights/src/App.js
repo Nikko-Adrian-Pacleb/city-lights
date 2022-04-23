69d768eec18bad0409bc7511feeccb8393139181
Nikko Pacleb <pacleb.nikko@gmail.com>
2022-04-22 23:33:34 -0700

    trial

---
+++ city-lights/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HomeBG from './components/HomeBG'
+
+function App() {
+  return (
+    <div>
+    <h1 className='hello'>Hello World</h1>
+      <HomeBG />
+    </div>
+  );
+}
+
+export default App;
